6b24851e6caeb5e2b263d4420103882990592eea
dthema <gleb.info2102@gmail.com>
2024-03-23 20:54:29 +0300

    connect nest with next

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
 
 @Module({
   imports: [],
   controllers: [AppController],
+  providers: [AppService],
+})
+@Module({
+  controllers: [AppController],
+  imports: [RenderModule.forRootAsync(Next({ dev: true }), { viewsDir: null })],
   providers: [AppService],
 })
 export class AppModule {}
